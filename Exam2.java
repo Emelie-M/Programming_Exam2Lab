8f96205e8b83081b6b552198aca4daded251c84b
emelie <ememejia2106@gmail.com>
2025-04-25 08:58:54 -0400

    Initial sharing of project

---
+++ Exam2.java
@@ -0,0 +1,49 @@
+
+/**
+ * Write a description of class Exam2 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Exam2
+{
+
+    /**
+     * Constructor for objects of class Exam2
+     */
+    public Exam2()
+    {
+        
+    }
+
+    /**
+     *Question 24
+     */
+    public void equalString()
+    {
+        String myStr1 = "Hello";
+        String myStr2 = "Byebye";
+        
+        //String myStr1.equals(myStr2); //outputs false
+    }
+    
+    /**
+     * Question 25
+     */
+    public void feverTemp(double temperature)
+    {
+        //ArrayList<double> arrayList = new arrayList<double>;
+        /*for each (int i = 0; temperature > 37.5; i++){
+            System.out.println(i);
+            System.out.println(temperature);
+        }*/
+        
+        //Question 26
+        //System.out.println(temperature.max);
+        
+        //Question 27
+        //temperatur.sort; //sort the temperature inputs
+        //System.out.println(temperature.sort(lastvalue)); //gets last temperature
+        
+    }
+}
